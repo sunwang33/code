6dab0e1a25fe35e54b5aa41a0daf7427ab7b0b09
sunwang33 <330463670@qq.com>
2017-08-13 11:36:17 +0800

    20170813b

--- s14/day04/decorator4.py
+++ s14/day04/decorator4.py
@@ -1,22 +1,29 @@
 __author__ = "sun wang"
 import time
-
-def deco(func):
-    start_time = time.time()
-    func()
-    stop_time = time.time()
-    print ("the run time of func is %s" % (start_time - stop_time))
-
+def timer(func):
+    def deco():
+        start_time = time.time()
+        func()
+        stop_time = time.time()
+        print ("the run time of func is %s" % (start_time - stop_time))
+    return deco
+@timer
 def test1():
     time.sleep(3)
     print ("in the test1")
-
+@timer
 def test2():
     time.sleep(3)
     print ("in the test2")
 
-deco(test1)
-deco(test2)
+#print(timer(test1))
+#test1 = timer(test1)
+test1()
+test2()
+# test1=deco(test1)
+# test1()
+# test2=deco(test2)
+# test2()
 #test1()
 #test2()
 
