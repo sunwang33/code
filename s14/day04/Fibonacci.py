f9b99c04b587f2cfd9930c0564c035cae7319eb4
sunwang33 <330463670@qq.com>
2017-08-14 22:30:02 +0800

    20170814a

--- s14/day04/Fibonacci.py
+++ s14/day04/Fibonacci.py
@@ -2,7 +2,28 @@ __author__ = "sun wang"
 def fib(max):
     n, a, b = 0, 0, 1
     while n < max:
-        print(b)
+        #print (b)
+        yield b
         a, b = b, a + b
         n = n + 1
     return 'done'
+g = fib(6)
+while True:
+
+    try:
+
+        x = next(g)  #这是一个内置方法和两个下划线效果一样。
+
+        print('g:', x)
+
+    except StopIteration as e:
+
+        print('Generator return value:', e.value)
+
+        break
+#print(fib(100))
+# f=fib(100)
+# print(f.__next__())
+# print(f.__next__())
+# print(f.__next__())
+# print(f.__next__())
