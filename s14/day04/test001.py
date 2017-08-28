196fe15af93c831fc93c3dfd839aca9f54e23de5
sunwang33 <330463670@qq.com>
2017-08-28 16:17:02 +0800

    20170828a

---
+++ s14/day04/test001.py
@@ -0,0 +1,18 @@
+__author__ = "sun wang"
+import time
+
+
+
+def timmer(func):
+    def warpper(*args , **kwargs):
+        start_time = time.time()
+        func()
+        stop_time = time.time()
+        print('the func run time is %s ' % (stop_time-start_time))
+    return warpper
+@timmer
+def test1():
+    time.sleep(3)
+    print("in the test1.")
+
+test1()
